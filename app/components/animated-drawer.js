030e280557dd4816090f84a4a2133eca825cf610
Matt Marcum <matt@strangelooper.com>
2015-08-10 01:10:27 -0500

    better var names

--- app/components/animated-drawer.js
+++ app/components/animated-drawer.js
@@ -5,15 +5,17 @@ const transitionEndEvent = 'transitionend';
 export default Ember.Component.extend({
   classNames: ['animation-drawer-container'],
   classNameBindings: ['open'],
+
   open: false,
-  isOpen: false,
+  renderDrawer: false,
+
   _onCloseDrawer: null,
   _setOnCloseDrawer: Ember.on('didInsertElement',
     function(){
       var _this = this;
       _this.set('_onCloseDrawer',
         Ember.run.bind(_this, function(){
-          _this.set('isOpen', false);
+          _this.set('renderDrawer', false);
           _this.$().off(transitionEndEvent,    this.get('_onCloseDrawer'));
         })
       );
@@ -22,7 +24,7 @@ export default Ember.Component.extend({
 
   actions: {
     openDrawer(){
-      this.set('isOpen', true);
+      this.set('renderDrawer', true);
       Ember.run.next(()=>{ this.set('open', true); });
     },
 
